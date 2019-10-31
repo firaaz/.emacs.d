ed0cb4c48ab49379cda0ce962e32b122814568e2
Firaaz <firaazfarook19@gmail.com>
2019-10-31 19:42:12 +0530

    cleanups

--- elpa/treemacs-evil-20190927.542/treemacs-evil-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "treemacs-evil" "20190927.542" "Evil mode integration for treemacs" '((evil "1.2.12") (treemacs "0.0")) :commit "6fb45a351b6d888fda44d5fc86b330b3ea54338f" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
--- elpa/treemacs-projectile-20190619.1516/treemacs-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "treemacs-projectile" "20190619.1516" "Projectile integration for treemacs" '((projectile "0.14.0") (treemacs "0.0")) :commit "6fb45a351b6d888fda44d5fc86b330b3ea54338f" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")

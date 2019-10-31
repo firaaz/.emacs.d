51cd935beea6c7842817743c6607e41da9a8d0e8
Firaaz <firaazfarook19@gmail.com>
2019-10-31 19:15:40 +0530

    helm -> ivy/counsel

--- init.el
+++ init.el
@@ -5,9 +5,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("428754d8f3ed6449c1078ed5b4335f4949dc2ad54ed9de43c56ea9b803375c23" default)))
  '(package-selected-packages
    (quote
-    (evil-commentary apropospriate-theme evil-unimpaired evil-surround quelpa-use-package quelpa all-the-icons treemacs-magit treemacs-projectile treemacs-evil treemacs smartparens rainbow-delimiters beacon helm-spotify-plus yasnippet-snippets yasnippet evil-matchit evil-magit evil-goggles helm-smex helm-fuzzier helm-flx helm-swoop smex evil-collection evil evil-mode company-box company-lsp lsp-ui lsp-mode company helm-projectile general helm magit which-key rtags use-package doom-themes))))
+    (counsel-spotify evil-commentary apropospriate-theme evil-unimpaired evil-surround quelpa-use-package quelpa all-the-icons treemacs-magit treemacs-projectile treemacs-evil treemacs smartparens rainbow-delimiters beacon helm-spotify-plus yasnippet-snippets yasnippet evil-matchit evil-magit evil-goggles helm-smex helm-fuzzier helm-flx helm-swoop smex evil-collection evil evil-mode company-box company-lsp lsp-ui lsp-mode company helm-projectile general helm magit which-key rtags use-package doom-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

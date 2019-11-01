dc7507ca7e611cae6f0df3a2987c6b783bae96cb
Firaaz <firaazfarook19@gmail.com>
2019-11-01 06:35:31 +0530

    org mode things

--- init.el
+++ init.el
@@ -8,10 +8,10 @@
  '(custom-enabled-themes (quote (doom-solarized-dark)))
  '(custom-safe-themes
    (quote
-    ("428754d8f3ed6449c1078ed5b4335f4949dc2ad54ed9de43c56ea9b803375c23" default)))
+	("428754d8f3ed6449c1078ed5b4335f4949dc2ad54ed9de43c56ea9b803375c23" default)))
  '(package-selected-packages
    (quote
-    (try general rustic web-mode js2-mode lsp-java lsp-python-ms smartparens rainbow-delimiters beacon treemacs-magit treemacs-projectile treemacs-evil treemacs helm-spotify-plus yasnippet-snippets yasnippet company-lsp lsp-ui lsp-mode company helm-projectile counsel helm flycheck-pos-tip flycheck evil-snipe evil-commentary evil-surround evil-matchit evil-magit evil-goggles evil-collection evil which-key doom-modeline doom-themes use-package rtags))))
+	(org-bullets evil-org org-pdfview pdf-tools try general rustic web-mode js2-mode lsp-java lsp-python-ms smartparens rainbow-delimiters beacon treemacs-magit treemacs-projectile treemacs-evil treemacs helm-spotify-plus yasnippet-snippets yasnippet company-lsp lsp-ui lsp-mode company helm-projectile counsel helm flycheck-pos-tip flycheck evil-snipe evil-commentary evil-surround evil-matchit evil-magit evil-goggles evil-collection evil which-key doom-modeline doom-themes use-package rtags))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

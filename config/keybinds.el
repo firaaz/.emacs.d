9a23193fd25be07dded8ab91aa3acf63428ff81f
firaaz <firaazfarook19@gmail.com>
2020-01-29 16:43:32 +0530

    treemacs and some refactoring

--- config/keybinds.el
+++ config/keybinds.el
@@ -1,5 +1,11 @@
 ;;; -*- lexical-binding: t -*-
 
+(use-package switch-window
+  :bind ("C-x o" . switch-window)
+  :config
+  (setq switch-window-shortcut-style 'qwerty
+        switch-window-qwerty-shortcuts '("a" "s" "d" "f" "g" "h" "j" "k" "l")))
+
 (defun my/fuzzy-project-file-finder (dir)
   "helper function for fuzzy finding of files"
   (counsel-projectile (cd dir)))
@@ -9,8 +15,6 @@
   (interactive)
   (my/fuzzy-project-file-finder "~/.emacs.d/"))
 
-
 (global-set-key (kbd "C-c f p") 'my/config-file)
 (global-set-key (kbd "C-x w") 'fixup-whitespace)
 ;; (global-set-key (kbd "C-c t l") (lambda () (interactive) (linum-mode))) ; to toggle line numbers (why is toggle not working)
-
--- config/theme.el
+++ config/theme.el
@@ -2,36 +2,27 @@
 (use-package which-key
   :init (which-key-mode))
 
-
+;; themes
 (use-package doom-themes)
+(use-package color-theme-sanityinc-tomorrow)
 
 (use-package doom-modeline
   :hook (after-init . doom-modeline-mode))
-
-;; (use-package ace-window
-;;   :config (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-;;   :bind ("C-x o" . ace-window))
-
-(use-package switch-window
-  :bind ("C-x o" . switch-window)
-  :config
-  (setq switch-window-shortcut-style 'qwerty
-        switch-window-qwerty-shortcuts '("a" "s" "d" "f" "g" "h" "j" "k" "l")))
-
 
 (use-package heaven-and-hell
   :init
   (setq heaven-and-hell-theme-type 'dark)
   (setq heaven-and-hell-themes
-	'((light . doom-solarized-light)
-	  (dark . doom-material)))
+	'((light . sanityinc-tomorrow-day)
+	  (dark . sanityinc-tomorrow-bright)))
   (setq heaven-and-hell-load-theme-no-confirm t)
   :hook (after-init . heaven-and-hell-init-hook)
   :bind ("C-C t t" . heaven-and-hell-toggle-theme))
 
 (use-package rainbow-mode)
 
-(use-package rainbow-delimiters)
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package vi-tilde-fringe
   :hook (prog-mode . vi-tilde-fringe-mode))
